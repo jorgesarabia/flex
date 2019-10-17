7c417eba0125a34269fd92e1acf8d3ec8e8e7cf9
jorgesarabia <jorge@breakdiving.org>
2019-10-17 11:41:39 -0300

    Se agregan las rutas dinamicas

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'events' => 'events#index'
-  get 'events/:id' => 'events#show'
+  get 'events/:id' => 'events#show', as: 'event'
 end
